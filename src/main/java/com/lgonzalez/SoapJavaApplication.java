a66d7984268d0d37d5a9f3f0a8131458d52b7ce1
programmingroup <luisgx14@gmail.com>
2024-12-03 16:44:41 -0500

    feat: :sparkles: Created soap application

---
+++ src/main/java/com/lgonzalez/SoapJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lgonzalez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapJavaApplication.class, args);
+    }
+
+}
